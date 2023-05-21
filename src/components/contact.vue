929840620f5baadaf55c6f671926abf3e422ddd8
Shauli Spitzer <sluahs@gmail.com>
2023-05-21 11:23:20 +0300

    made contact form responsive

--- src/components/contact.vue
+++ src/components/contact.vue
@@ -5,15 +5,15 @@ const emit = defineEmits(["back"]);
   <div class="buttonDiv w-full flex justify-center">
     <button @click="$emit('back')">back</button>
   </div>
-  <iframe
-    src="/contactUs.html"
-    width="50%"
-    height="1200px"
-    sandbox="allow-forms allow-popups allow-pointer-lock allow-scripts"
-  ></iframe>
-  <p class="text w-48 float-right">
+  <p class="text md:w-48 w-full md:float-right float-none">
     Thanks for visiting Pet Shop! We'd love to hear from you. Please reach out to us if
     you have any questions, comments, or concerns about our pet shop and the services we
     offer. We look forward to hearing from you soon.
   </p>
+  <iframe
+    src="/contactUs.html"
+    class="md:w-1/2 w-full"
+    height="1200px"
+    sandbox="allow-forms allow-popups allow-pointer-lock allow-scripts"
+  ></iframe>
 </template>
