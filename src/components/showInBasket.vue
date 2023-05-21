3732cd087b9a655ab04634c6189e7c1f60d8a80d
Shauli Spitzer <sluahs@gmail.com>
2023-05-21 16:14:45 +0300

    capitalize

--- src/components/showInBasket.vue
+++ src/components/showInBasket.vue
@@ -3,8 +3,8 @@ const props=defineProps({totalPrice:String,amount:Number})
 </script>
 <template>
     <div class="h-10 w-16 bg-blue-500 text-white text-[10px] font-bold rounded-tr-lg">
-        <span class="font-bold">ordered: {{ amount }}</span><br>
-        <span>total:</span><br>
+        <span class="font-bold">Ordered: {{ amount }}</span><br>
+        <span>Total:</span><br>
         <span class="leading-3">{{ totalPrice }}</span> 
         </div>
 </template>
